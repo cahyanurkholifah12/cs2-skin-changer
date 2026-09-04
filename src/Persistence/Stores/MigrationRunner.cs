e433626b48f4546f41a8b004ff11686de9771b85
cahyanurkholifah12 <cahyanurkholifah12@gmail.com>
2026-09-04 23:27:27 +0000

    Initial commit: src/Persistence/Stores/MigrationRunner.cs

---
+++ src/Persistence/Stores/MigrationRunner.cs
@@ -0,0 +1,24 @@
+using Cs2Skins.Persistence.Schema;
+using Microsoft.Data.Sqlite;
+
+namespace Cs2Skins.Persistence.Stores;
+
+public sealed class MigrationRunner
+{
+    private readonly SqliteConnection _connection;
+
+    public MigrationRunner(SqliteConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public async Task ApplyAsync(CancellationToken cancellationToken)
+    {
+        foreach (var statement in SchemaDefinitions.AllMigrations)
+        {
+            await using var command = _connection.CreateCommand();
+            command.CommandText = statement;
+            await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
